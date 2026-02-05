cbbba8abc9b1cbf6f47d5b56cab584f7417044b0
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-05 17:33:14 +0530

    initial commit

---
+++ src/main/java/com/pramodvaddiraju/TaskVerse/TaskVerseApplication.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.TaskVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskVerseApplication.class, args);
+	}
+
+}
